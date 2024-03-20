f068d8e9b5ae0edf97700eeb7ecbf3ad13874677
zzoo <zzooxox@gmail.com>
2024-03-20 23:46:06 +0900

    Add login functionality to API server

--- api/server.go
+++ api/server.go
@@ -5,34 +5,42 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/zzooman/zapp-server/db/sqlc"
+	"github.com/zzooman/zapp-server/token"
 )
 type Server struct {
 	store db.Store
+	tokenMaker token.Maker
 	router *gin.Engine
 }
 
 func NewServer(store db.Store) *Server {
 	server := &Server{
 		store: store, 
+		tokenMaker: token.NewPasetoMaker(),
 		router: gin.Default(),
-	}	
+	}			
 	
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	server.router.POST("/user", server.createUser)
-	server.router.GET("/user/:id", server.getUser)
-	server.router.DELETE("/user/:id", server.deleteUser)
-
-	server.router.POST("/account", server.createAccount)
-	server.router.GET("/account/:id", server.getAccount)
-	server.router.DELETE("/account/:id", server.deleteAccount)
-
-	server.router.POST("/transfer", server.createTransfer)
-	server.router.GET("/transfer/:id", server.getTransfer)
-
+	server.setUpRouter(server.router)
 	return server
+}
+
+func (server *Server) setUpRouter(router *gin.Engine) {
+	router.POST("/login", server.loginUser)
+	
+	router.POST("/user", server.createUser)
+	router.GET("/user/:id", server.getUser)
+	router.DELETE("/user/:id", server.deleteUser)	
+
+	router.POST("/account", server.createAccount)
+	router.GET("/account/:id", server.getAccount)
+	router.DELETE("/account/:id", server.deleteAccount)
+
+	router.POST("/transfer", server.createTransfer)
+	router.GET("/transfer/:id", server.getTransfer)
 }
 
 func (server *Server) Start(address string) error {
--- api/user.go
+++ api/user.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgtype"
@@ -24,6 +25,17 @@ const (
 	LocationGyeonggi Location = "Gyeonggi"
 	// Add more location options as needed
 )
+func newUserResponse(user db.User) userResponse {
+	return userResponse{
+		Username:          user.Username,
+		Email:             user.Email,
+		Phone:             user.Phone,
+		Location:          user.Location,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+}
+
 type createUserRequest struct {
 	Username string   `json:"username" binding:"required,alphanum,min=4,max=32"`
 	Password string   `json:"password" binding:"required,min=6,max=32"`
@@ -31,7 +43,7 @@ type createUserRequest struct {
 	Phone    string   `json:"phone" binding:"omitempty,phone"`
 	Location Location `json:"location" binding:"required"`
 }
-type createUserResponse struct {
+type userResponse struct {
 	Username          string             `json:"username"`	
 	Email             string             `json:"email"`
 	Phone             pgtype.Text        `json:"phone"`
@@ -65,14 +77,7 @@ func (server *Server) createUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	rsp := createUserResponse{
-		Username:          user.Username,
-		Email:             user.Email,
-		Phone:             user.Phone,
-		Location:          user.Location,
-		PasswordChangedAt: user.PasswordChangedAt,
-		CreatedAt:         user.CreatedAt,		
-	}
+	rsp := newUserResponse(user)
 	ctx.JSON(http.StatusOK, rsp)
 }
 
@@ -123,4 +128,42 @@ func (server *Server) deleteUser(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, user)
+}
+
+
+// Login User
+type loginUserRequest struct {
+	Username string   `json:"username" binding:"required,alphanum,min=4,max=32"`
+	Password string   `json:"password" binding:"required,min=6,max=32"`
+}
+type loginUserResponse struct {
+	AccessToken string 		  `json:"access_token"`
+	User 	   	userResponse  `json:"user"`
+}
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	if(!utils.CheckPasswordHash(req.Password, user.Password)) {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+
+	accessToken, err := server.tokenMaker.CreateToken(user.Username, time.Duration(time.Duration.Minutes(720)))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	rsp := loginUserResponse{
+		AccessToken: accessToken,
+		User: newUserResponse(user),
+	}
+	ctx.JSON(http.StatusOK, rsp)
 }

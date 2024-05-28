d810e25dfc4901d020598a52747c3f7f28696b96
zzoo <zzooxox@gmail.com>
2024-05-29 00:51:49 +0900

    feat: Add endpoints for creating and deleting likes

---
+++ api/like.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/zzooman/zapp-server/db/sqlc"
+	"github.com/zzooman/zapp-server/token"
+)
+
+type createLikeRequest struct {
+	PostID int64 `json:"post_id" binding:"required"`
+}
+func (server *Server) createLike(ctx *gin.Context) {
+	var req createLikeRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	authPayload := ctx.MustGet(AUTH_TOKEN).(*token.Payload)
+	like, err := server.store.CreateLikeWithPost(ctx, db.CreateLikeWithPostParams{
+		PostID: req.PostID,
+		Username: authPayload.Username,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, like)
+}
+
+type deleteLikeRequest struct {
+	PostID int64 `json:"post_id" binding:"required"`
+}
+func (server *Server) deleteLike(ctx *gin.Context) {
+	var req deleteLikeRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	authPayload := ctx.MustGet(AUTH_TOKEN).(*token.Payload)
+	err := server.store.DeleteLikeWithPost(ctx, db.DeleteLikeWithPostParams{
+		PostID: req.PostID,
+		Username: authPayload.Username,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, map[string]string{"message": "success"})
+}

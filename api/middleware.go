1819ab1aa5ea259e1269f9a05c4185e3ae2d884e
zzoo <zzooxox@gmail.com>
2024-06-16 17:17:10 +0900

    refactor: Update authMiddleware to handle token from Authorization header

--- api/middleware.go
+++ api/middleware.go
@@ -3,6 +3,7 @@ package api
 import (
 	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zzooman/zapp-server/token"
@@ -15,21 +16,53 @@ const (
 func authMiddleware(tokenMaker token.Maker) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		tokenInCookie, err := ctx.Cookie(AUTH_TOKEN)
-		if err != nil {
-			ctx.JSON(http.StatusUnauthorized, errorResponse(errors.New("auth_token is empty")))
-			ctx.Abort()
-			return
-		}
-		token, err := tokenMaker.VerifyToken(tokenInCookie)
-		if err != nil {
-			// Remove the auth_token cookie
-			ctx.SetCookie(AUTH_TOKEN, "", -1, "", "", false, true)
-			ctx.JSON(http.StatusUnauthorized, errorResponse(err))
-			ctx.Abort()			
-			return
-		}
+		if tokenInCookie != "" {
+			if err != nil {
+				ctx.JSON(http.StatusUnauthorized, errorResponse(errors.New("auth_token is empty")))
+				ctx.Abort()
+				return
+			}
+			token, err := tokenMaker.VerifyToken(tokenInCookie)
+			if err != nil {
+				// Remove the auth_token cookie
+				ctx.SetCookie(AUTH_TOKEN, "", -1, "", "", false, true)
+				ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+				ctx.Abort()			
+				return
+			}
+			ctx.Set(AUTH_TOKEN, token)
+			ctx.Next()
+		} else {
+			// Parse the token from the Authorization header
+			authHeader := ctx.GetHeader("Authorization")
+			if authHeader == "" {
+				ctx.JSON(http.StatusUnauthorized, errorResponse(errors.New("Authorization header is missing")))
+				ctx.Abort()
+				return
+			}
 
-		ctx.Set(AUTH_TOKEN, token)
-		ctx.Next()
+			// Split the header value to get the token
+			splitToken := strings.Split(authHeader, "Bearer ")
+			if len(splitToken) != 2 {
+				ctx.JSON(http.StatusUnauthorized, errorResponse(errors.New("Invalid token format")))
+				ctx.Abort()
+				return
+			}
+
+			// Get the token value
+			tokenValue := splitToken[1]
+
+			// Verify the token
+			token, err := tokenMaker.VerifyToken(tokenValue)
+			if err != nil {
+				// Remove the auth_token cookie
+				ctx.SetCookie(AUTH_TOKEN, "", -1, "", "", false, true)
+				ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+				ctx.Abort()
+				return
+			}			
+			ctx.Set(AUTH_TOKEN, token)
+			ctx.Next()
+		}		
 	}
 }
